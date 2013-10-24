9b4f30e59ca2a624a50450fb75840c63b1a14524
Graham Cantin <Kamilion@gmail.com>
2013-10-24 14:01:46 -0700

    add a sudo helper so diskaction can be triggered from a desktop.

--- disker-terminal.sh
+++ disker-terminal.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+# Make sure we're in the right directory.
 cd /home/git/disker-gui/
+# -n will disable the password prompt, -s will create a shelled session.
 sudo -ns /home/git/disker-gui/diskaction.py
