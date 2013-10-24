16e9f1dcb6b1f998ecd40fa658a2419503215384
Graham Cantin <Kamilion@gmail.com>
2013-10-24 15:09:40 -0700

    Enable force mode from the helper.

--- disker-terminal.sh
+++ disker-terminal.sh
@@ -2,4 +2,4 @@
 # Make sure we're in the right directory.
 cd /home/git/disker-gui/
 # -n will disable the password prompt, -s will create a shelled session.
-sudo -ns /home/git/disker-gui/diskaction.py
+sudo -ns /home/git/disker-gui/diskaction.py -f
